acdec2b4ada0b3d831d369c3e94ad02124f7d76c
Emilia Kond <emilia@rymiel.space>
2025-05-28 19:59:27 +0300

    Add error parameter to UserOnly wrapper

--- src/components/UserOnly.tsx
+++ src/components/UserOnly.tsx
@@ -3,12 +3,12 @@ import { PropsWithChildren, useContext } from "react";
 
 import { User } from "../providers/user";
 
-export default function UserOnly({ children }: PropsWithChildren) {
+export default function UserOnly({ children, error = false }: PropsWithChildren<{ error?: boolean }>) {
   const { user } = useContext(User);
 
-  if (!user) {
+  if (!user && error) {
     return <NonIdealState icon="error" title="You cannot access this page" />;
+  } else if (user !== null) {
+    return children;
   }
-
-  return children;
 }
