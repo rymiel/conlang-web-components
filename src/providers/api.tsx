2aeba550f3c02f86048c9ba76a2e564618638931
Emilia Kond <emilia@rymiel.space>
2025-05-28 19:43:07 +0300

    Store error handler

--- src/providers/api.tsx
+++ src/providers/api.tsx
@@ -1,20 +1,26 @@
 import { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
 
 import { ApiClient } from "../api";
+import { ErrorHandler } from "./shared";
 
 export const ApiVersion = createContext<string | null>(null);
-export const Api = createContext<ApiClient | null>(null);
 
-export function ApiProvider({ children, api }: PropsWithChildren<{ api: ApiClient }>) {
+interface ApiData {
+  client: ApiClient;
+  error: ErrorHandler;
+}
+const Api = createContext<ApiData | null>(null);
+
+export function ApiProvider({ children, api, error }: PropsWithChildren<{ api: ApiClient; error: ErrorHandler }>) {
   const [version, setVersion] = useState<string | null>(null);
   useEffect(() => {
     api
       .version()
       .then((text) => setVersion(text))
-      .catch((err) => console.error(err));
+      .catch((err) => error(err));
   }, []);
 
-  return <Api.Provider value={api}>
+  return <Api.Provider value={{ client: api, error }}>
     <ApiVersion.Provider value={version}>{children}</ApiVersion.Provider>
   </Api.Provider>;
 }
@@ -24,5 +30,13 @@ export function useApi(): ApiClient {
   if (api === null) {
     throw new Error("No API initialized");
   }
-  return api;
+  return api.client;
+}
+
+export function useErrorHandler(): ErrorHandler {
+  const api = useContext(Api);
+  if (api === null) {
+    throw new Error("No API initialized");
+  }
+  return api.error;
 }
--- src/providers/shared.tsx
+++ src/providers/shared.tsx
@@ -6,22 +6,26 @@ import { Dictionary, DictionaryData } from "./dictionary";
 import { Lang, LangData } from "./langConfig";
 import { Title } from "./title";
 
+export type ErrorHandler = (error: unknown) => void;
+
 export function ConlangProvider<D extends DictionaryData, L extends LangData>({
   children,
   dictionary,
   lang,
   api,
+  error,
 }: {
   children: ReactNode;
   dictionary: Context<D>;
   lang: Context<L | null>;
   api: ApiClient;
+  error: ErrorHandler;
 }) {
   const dictionaryValue = useContext(dictionary);
   const langValue = useContext(lang);
   const [title, setTitle] = useState<string | null>(null);
 
-  return <ApiProvider api={api}>
+  return <ApiProvider api={api} error={error}>
     <Lang.Provider value={langValue}>
       <Dictionary.Provider value={dictionaryValue}>
         <Title.Provider value={{ title, setTitle }}>{children}</Title.Provider>
